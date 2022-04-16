43feea5929f0026ab41f53a8029fdd0904c06595
vnovopashin <novopashinva@gmail.com>
2022-04-16 21:31:46 +0700

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,6 +3,12 @@ package ru.job4j.concurrent;
 /**
  * Класс демонстрирует пример прерывания нити, метод interrupt() выставляет флаг прерывания,
  * но ни каких действий не совершает, для проверки флага воспользуемся методом isInterrupted().
+ * Метод Thread.interrupt() не выставляет флаг прерывания, если нить находится в режиме WAIT, JOIN.
+ * В этом случае методы sleep(), join(), wait() выкинут исключение.
+ * Поэтому нужно дополнительно проставить флаг прерывания.
+ * В блоке catch нужно дополнительно вызывать прерывание нити для того чтобы прерывание действительно произошло.
+ * Эта схема является шаблоном. Запомните, если используются методы sleep(), join() или wait(),
+ * то нужно в блоке catch вызвать прерывание.
  *
  * @author Vasiliy Novopashin
  * @version 1.0
@@ -20,7 +26,6 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                e.printStackTrace();
                 Thread.currentThread().interrupt();
             }
         }
